5bea4c8c3e209533aa5e23c119303e10cf24ba1c
Salvatore <dested@gmail.com>
2013-05-19 11:39:52 -0700

    included the webform project

---
+++ spoke1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ConsoleApplication1;
+using Timer = System.Threading.Timer;
+
+namespace spoke1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.UpdateStyles();
+
+            this.Paint += Form1_Paint;
+            this.Load += Form1_Load;
+            this.MouseDown += Form1_MouseDown;
+            this.MouseUp += Form1_MouseUp;
+            this.MouseMove += Form1_MouseMove;
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private Point MousePoint;
+        private bool isMouseDown;
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            MousePoint = e.Location;
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+            var l = new liqud(MouseStatus);
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += l.run;
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerAsync();
+
+        }
+
+        public Tuple<Point,bool> MouseStatus()
+        {
+            return new Tuple<Point, bool>(MousePoint,isMouseDown);
+        }
+        
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lines = ((List<Tuple<PointF, PointF>>) e.UserState);
+            ticks++;
+            Invalidate();
+
+
+        }
+         
+
+        private List<Tuple<PointF, PointF>> lines;
+        private int ticks = 0;
+        
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Black, e.ClipRectangle);
+            e.Graphics.DrawString("frame "+ticks, this.Font, Brushes.White, 0, 0);
+            if (lines!=null)
+            {
+           
+                for (int index = lines.Count-1; index >= 0; index--)
+                {
+                    var line = lines[index];
+                    if (line.Item1 == line.Item2)
+                    {  e.Graphics.DrawLine(new Pen(Color.Red), line.Item1, new PointF(line.Item2.X+0.01f, line.Item2.Y));
+
+                    }
+                    else
+                     //   e.Graphics.FillRectangle((new SolidBrush(Color.Blue)), new RectangleF(line.Item1, new SizeF(line.Item2.X - line.Item1.X, line.Item2.Y - line.Item1.Y)));
+                    e.Graphics.DrawLine(new Pen(Color.Blue), line.Item1, line.Item2);
+                }
+                lines.Clear();
+
+            }
+        }
+    }
+}
---
+++ spoke1/liqud.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ConsoleApplication1;
+
+namespace spoke1
+{
+    public class liqud
+    {
+        private readonly Func<Tuple<Point, bool>> _mouseStatus;
+
+
+        public void run(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+
+            
+
+            var rv = new Dictionary
+<string, Func<SpokeObject[], SpokeObject>>()
+                         {
+                             {
+                                 "write", (a) =>
+                                              {
+                                                  for (int index = 1; index < a.Length; index++)
+                                                  {
+                                                      var spokeObject = a[index];
+                                                      Console.Write(spokeObject.ToString() + " ");
+                                                  }
+                                                  return null;
+                                              }
+                                 },
+                             {
+                                 "readLine", (a) =>
+                                                 {
+                                                     return new SpokeObject()
+                                                                {
+                                                                    Type = ObjectType.String,
+                                                                    StringVal = Console.ReadLine()
+                                                                };
+                                                 }
+                                 },
+                             {
+                                 "read", (a) =>
+                                             {
+                                                 return new SpokeObject()
+                                                            {
+                                                                Type = ObjectType.String,
+                                                                StringVal = Console.Read().ToString()
+                                                            };
+                                             }
+                                 },
+                             {
+                                 "stringToInt", (a) =>
+                                                    {
+                                                        return new SpokeObject()
+                                                                   {
+                                                                       Type = ObjectType.Int,
+                                                                       IntVal = int.Parse(a[1].StringVal)
+                                                                   };
+                                                    }
+                                 },{
+                                 "floatToInt", (a) =>
+                                                    {
+                                                        return new SpokeObject()
+                                                                   {
+                                                                       Type = ObjectType.Int,
+                                                                       IntVal = (int)a[1].FloatVal
+                                                                   };
+                                                    }
+                                 },
+                             {
+                                 "debug", (a) =>
+                                              {
+                                                  return null;
+                                              }
+                                 },
+                             {
+                                 "writeLine", (a) =>
+                                                  {
+                                                      for (int index = 1; index < a.Length; index++)
+                                                      {
+                                                          var spokeObject = a[index];
+                                                          Console.Write(spokeObject.ToString() + " ");
+                                                      }
+                                                      Console.Write("\r\n");
+                                                      return null;
+                                                  }
+                                 },
+                             {
+                                 "clearConsole", (a) =>
+                                                     {
+                                                         Console.Clear();
+                                                         return null;
+                                                     }
+                                 },
+                             {
+                                 "stringLength", (a) =>
+                                                     {
+                                                         return new SpokeObject()
+                                                                    {
+                                                                        Type = ObjectType.Int,
+                                                                        IntVal = a[1].StringVal.Length
+                                                                    };
+                                                     }
+                                 },
+                             {
+                                 "setConsolePosition", (a) =>
+                                                           {
+                                                               Console.SetCursorPosition(a[1].IntVal, a[2].IntVal);
+                                                               return null;
+                                                           }
+                                 },
+                             
+                             {
+                                 "abs", (a) =>
+                                            {
+                                                switch (a[1].Type)
+                                                {
+                                                    case ObjectType.Int: var c = a[1].IntVal;
+                                                        return new SpokeObject() { IntVal = Math.Abs(c), Type = ObjectType.Int };
+
+                                                        break;
+                                                    case ObjectType.Float: var cd = a[1].FloatVal;
+                                                        return new SpokeObject() { FloatVal = Math.Abs(cd),Type=ObjectType.Float };
+
+                                                        break;
+                                                    default:
+                                                        throw new ArgumentOutOfRangeException();
+                                                }
+                                            }
+                                 },
+                             {
+                                 "nextRandom", (a) =>
+                                                   {
+                                                       return new SpokeObject()
+                                                                  {
+                                                                      Type = ObjectType.Int,
+                                                                      IntVal = rad.Next(a[1].IntVal, a[2].IntVal)
+                                                                  };
+                                                       return null;
+                                                   }
+                                 },
+                             {
+                                 "rand", (a) =>
+                                             {
+                                                 var vfd = new SpokeObject() { Type = ObjectType.Float, FloatVal = (float)rad.NextDouble() };
+                                                 return vfd;
+                                             }
+                                 }, {
+                                 "getMouseX", (a) =>
+                                             {
+                                                 var vfd = new SpokeObject() { Type = ObjectType.Int, IntVal = _mouseStatus().Item1.X };
+                                                 return vfd;
+                                             }
+                                 }, {
+                                 "getMouseY", (a) =>
+                                             {
+                                                 var vfd = new SpokeObject() { Type = ObjectType.Int, IntVal = _mouseStatus().Item1.Y };
+                                                 return vfd;
+                                             }
+                                 }, {
+                                 "getMouseClicked", (a) =>
+                                             {
+                                                 var vfd = new SpokeObject() { Type = ObjectType.Bool, BoolVal = _mouseStatus().Item2 };
+                                                 return vfd;
+                                             }
+                                 }
+                                 
+                                 
+                                 ,{
+                                 "line", (a) =>
+                                             {
+                                                 lines.Add(new Tuple<PointF, PointF>(new PointF(a[1].FloatVal + 10, a[2].FloatVal + 10), new PointF(a[3].FloatVal + 10, a[4].FloatVal + 10)));
+                                                 return null;
+                                             }
+                                 },{
+                                 "wait", (a) =>
+                                             {
+                                                 ((BackgroundWorker)sender).ReportProgress(0,lines);
+                                                 lines=new List<Tuple<PointF, PointF>>();
+                                                 return null;
+                                             }
+                                 }, 
+                         };
+
+
+
+
+            lines = new List<Tuple<PointF, PointF>>();
+
+            try
+            {
+                var ra = new RunApp(@"liquid.spoke");
+                ((BackgroundWorker)sender).ReportProgress(0,lines);
+              //  ra.run(rv);
+
+                Console.Write("DonSe");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er);
+
+                Application.Exit();
+
+            }
+
+
+
+
+        }
+        static Random rad = new Random();
+        private List<Tuple<PointF, PointF>> lines;
+
+        public liqud(Func<Tuple<Point, bool>> mouseStatus)
+        {
+            _mouseStatus = mouseStatus;
+        }
+    }
+}
